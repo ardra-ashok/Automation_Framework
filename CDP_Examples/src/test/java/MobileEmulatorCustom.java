f6ceb2c89d9f6e2cf09c9d40548d57ed53da15d6
ardra-ashok <ardraasok@yahoo.in>
2025-07-23 13:11:04 +1000

    added custom code for Mobile emulator CDP

--- CDP_Examples/src/test/java/MobileEmulatorCustom.java
+++ CDP_Examples/src/test/java/MobileEmulatorCustom.java
@@ -1,26 +1,32 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.DevTools;
-import org.openqa.selenium.devtools.v123.emulation.Emulation;
 import org.testng.annotations.Test;
 
-import java.util.Optional;
+import java.util.HashMap;
+import java.util.Map;
 
-public class MobileEmulatorTest {
+public class MobileEmulatorCustom {
 
     @Test
-    public void testEmulator() throws InterruptedException {
+    public void MobileTest() throws InterruptedException {
+
         System.setProperty("webdriver.chrome.driver","/Users/aashok/IdeaProjects/Automation_Framework/supportData/drivers/chromedriver");
         ChromeDriver webDriver = new ChromeDriver();
         DevTools devTools = webDriver.getDevTools();
         devTools.createSession();
-        devTools.send(Emulation.setDeviceMetricsOverride(600, 1000, 50, true, Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty()));
+        Map deviceMetrics = new HashMap<>();
+        deviceMetrics.put("width",600);
+        deviceMetrics.put("height",600);
+        deviceMetrics.put("deviceScaleFactor",50);
+        deviceMetrics.put("mobile",true);
+
+        webDriver.executeCdpCommand("Emulation.setDeviceMetricsOverride",deviceMetrics);
         webDriver.get("https://rahulshettyacademy.com/angularAppdemo/");
         webDriver.findElement(By.cssSelector(".navbar-toggler")).click();
         Thread.sleep(3000);
         webDriver.findElement(By.linkText("Library")).click();
 
         webDriver.quit();
-
     }
 }
