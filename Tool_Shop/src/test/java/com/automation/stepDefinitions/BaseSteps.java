1e7544831742d237cd14c12007426766c277f395
ardra-ashok <ardraasok@yahoo.in>
2025-08-18 21:42:00 +1000

    Added logging to the steps

--- Tool_Shop/src/test/java/com/automation/stepDefinitions/BaseSteps.java
+++ Tool_Shop/src/test/java/com/automation/stepDefinitions/BaseSteps.java
@@ -6,12 +6,15 @@ import configs.PropertyManager;
 import core.WebDriverHandler;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
-import utils.ReportHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class BaseSteps extends Helpers {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BaseSteps.class);
     private WebDriverHandler webDriverHandler;
     private ScenarioContext scenarioContext;
 
@@ -37,19 +40,22 @@ public class BaseSteps extends Helpers {
     @When("^I launch \"([^\"]*)\" website$")
     public void i_launch_website(String url) {
         String baseUrl = replaceParamWithVariable(PropertyManager.getEnvironmentVariable(url));
+        LOG.info(String.format("Launching url: "+baseUrl));
         webDriverHandler.navigateToUrl(baseUrl);
-//        scenarioContext.setStepDescription(String.format("I navigated to "));
+        scenarioContext.setStepDescription(String.format("I navigated to "+baseUrl));
     }
 
     @Given("^I set the test environment to \"([^\"]*)\"$")
     public void i_set_the_test_environment_to(String env) throws Exception {
         env = replaceParamWithVariable(env);
+        LOG.info(String.format("Set the test environment: "+env));
         PropertyManager.loadTestPropertiesFromYML(env);
         scenarioContext.setStepDescription(String.format("I set the environment to "+env));
     }
 
     @When("I save the value of {string} in variable {string}")
     public void iSaveTheValueOfInVariable(String inputVariable, String variableName) {
+        LOG.info(String.format("Saving the value of : "+inputVariable+" in " +variableName));
         PropertyManager.setScenarioVariable(variableName,replaceParamWithVariable(inputVariable));
     }
 }
